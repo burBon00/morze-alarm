572a50caf8c3e912f9fe3342862ff685a0d222eb
André Tvard <andtva@piab.com>
2026-01-28 20:13:49 +0100

    First RTC working

--- morze-alarm.ino
+++ morze-alarm.ino
@@ -6,12 +6,17 @@
 
 #include <Keypad.h>
 
-#include <ClearDS1302.h>
+#include <RtcDS1302.h>
 
-int RTCrstPin = A3;
-int RTCclkPin = A1;
-int RTCdatPin = A2;
-ClearDS1302 RTC1 = ClearDS1302(RTCdatPin, RTCrstPin, RTCclkPin);
+
+// CONNECTIONS:
+// DS1302 CLK/SCLK --> 5
+// DS1302 DAT/IO --> 4
+// DS1302 RST/CE --> 2
+// DS1302 VCC --> 3.3v - 5v
+// DS1302 GND --> GND
+ThreeWire myWire(A2, A1, A3);  // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
@@ -20,16 +25,16 @@ ClearDS1302 RTC1 = ClearDS1302(RTCdatPin, RTCrstPin, RTCclkPin);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Four columns
+const byte ROWS = 4;  // Four rows
+const byte COLS = 4;  // Four columns
 char keys[ROWS][COLS] = {
-  {'A','3','2','1'},
-  {'B','6','5','4'},
-  {'C','9','8','7'},
-  {'D','#','0','*'}
+  { 'A', '3', '2', '1' },
+  { 'B', '6', '5', '4' },
+  { 'C', '9', '8', '7' },
+  { 'D', '#', '0', '*' }
 };
-byte rowPins[ROWS] = {2, 3, 4, 5}; // Connect to the row pinouts of the keypad
-byte colPins[COLS] = {6, 7, 8, 9}; // Connect to the column pinouts of the keypad
+byte rowPins[ROWS] = { 2, 3, 4, 5 };  // Connect to the row pinouts of the keypad
+byte colPins[COLS] = { 6, 7, 8, 9 };  // Connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
@@ -41,7 +46,7 @@ int min = 0;
 int sec = 0;
 int msec = 0;
 
-unsigned long lastTick=0;
+unsigned long lastTick = 0;
 
 #define STATE_IDLE 0
 #define STATE_EDIT 1
@@ -50,71 +55,93 @@ unsigned long lastTick=0;
 int current_state = 0;
 char current_state_name[20];
 
-void setup()
-{
 
-  Serial.begin(9600);
-  
-  Serial.println("Starting...");
+void setDate() {
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
 
+  Rtc.Begin();
 
-  pinMode(PIN_BUZZER, 11);
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
 
- // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+  if (!Rtc.IsDateTimeValid()) {
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+
+    Serial.println("RTC lost confidence in the DateTime!");
+    Rtc.SetDateTime(compiled);
   }
 
-  display.display();
+  if (Rtc.GetIsWriteProtected()) {
+    Serial.println("RTC was write protected, enabling writing now");
+    Rtc.SetIsWriteProtected(false);
+  }
 
-  display.clearDisplay();
+  if (!Rtc.GetIsRunning()) {
+    Serial.println("RTC was not actively running, starting now");
+    Rtc.SetIsRunning(true);
+  }
 
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
-  display.print(hour);
-  display.print(":");
-  display.print(min);
-  display.print(":");
-  display.print(sec);
-  display.print(".");
-  display.print(msec);
-
-  display.display();
-
-  //RTC1.set.time.SetAll(int second, int minute, int hour, int day, int date, int month, int year, bool ClockRegister)
-  RTC1.set.time.SetAll(0, 30, 12, 2, 26, 7, 2025, false);
-
-  delay(10);
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled) {
+    Serial.println("RTC is older than compile time!  (Updating DateTime)");
+    Rtc.SetDateTime(compiled);
+  } else if (now > compiled) {
+    Serial.println("RTC is newer than compile time. (this is expected)");
+  } else if (now == compiled) {
+    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
 }
 
-  int DISPLAY_INTERVAL = 100;
-  unsigned long display_updated = 0;
+void setup() {
 
+  Serial.begin(9600);
+
+  Serial.println("Starting...");
+
+  setDate();
+  pinMode(PIN_BUZZER, 11);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+
+  display.setTextColor(SSD1306_WHITE);
+}
+
+int DISPLAY_INTERVAL = 1000;
+unsigned long display_updated = 0;
 char pressed_key;
 
-char time_str[100];
-void loop()
-{
+void printDigits(uint8_t digits) {
+  if (digits < 10) {
+    display.print('0');
+  }
+  display.print(digits);
+}
 
-char key1 = keypad.getKey();
+void loop() {
+
+  char key1 = keypad.getKey();
   if (key1 != NO_KEY) {
     Serial.print("Key: ");
     Serial.println(key1);
     pressed_key = key1;
   }
 
-  if(pressed_key == 'C')
-  {
+  if (pressed_key == 'C') {
     Serial.println("Buzzer ON");
     tone(PIN_BUZZER, 100);
-  }
-  else
-  {
-   Serial.println("Buzzer OFF");
-   noTone(PIN_BUZZER);
+  } else {
+    //Serial.println("Buzzer OFF");
+    noTone(PIN_BUZZER);
   }
 
   // char key = keypad.getKey();
@@ -125,52 +152,29 @@ char key1 = keypad.getKey();
   //   }
   // }
 
-  unsigned long now = millis();
 
-   if (now - lastTick >= 10)
-  {
-    lastTick += 10;   // ВАЖНО: не now, а +1
-    msec+=10;
-  }
+  if (millis() > display_updated + DISPLAY_INTERVAL) {
 
-  if (msec >= 100)
-  {
-    msec = 0;
-    sec++;
-  }
+    RtcDateTime nowTime = Rtc.GetDateTime();
 
-  if (sec >= 60)
-  {
-    sec = 0;
-    min++;
-  }
+    printDateTime(nowTime);
+    Serial.println();
 
-  if (min >= 60)
-  {
-    min = 0;
-    hour++;
-  }
-
-  if (hour >= 24)
-  {
-    hour = 0;
-  }
-  
-
-
-  if(millis() > display_updated + DISPLAY_INTERVAL){
-
-    display.setCursor(0,0);
+    display.setCursor(17, 0);
     display.clearDisplay();
-    display.print(hour);
+    display.setTextSize(2);
+    printDigits(nowTime.Hour());
     display.print(":");
-    display.print(min);
+    printDigits(nowTime.Minute());
     display.print(":");
-    display.print(sec);
-    display.print(".");
-    display.print(msec);
+    printDigits(nowTime.Second());
+    display.setTextSize(1);
+    if (!nowTime.IsValid()) {
+      Serial.println("RTC lost confidence in the DateTime!");
+      display.print("<- ERROR!");
+    }
 
-    display.setCursor(0,20);
+    display.setCursor(0, 20);
     display.print("Pressed: ");
     display.print(pressed_key);
 
@@ -178,10 +182,22 @@ char key1 = keypad.getKey();
     display.display();
 
     display_updated = millis();
-
-  byte * timestr1 = RTC1.get.time.GetAll();
-
-    Serial.println((char *) timestr1);
   }
+}
 
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt) {
+  char datestring[26];
+
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+             dt.Month(),
+             dt.Day(),
+             dt.Year(),
+             dt.Hour(),
+             dt.Minute(),
+             dt.Second());
+  Serial.print(datestring);
 }
